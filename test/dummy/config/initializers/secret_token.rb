4c05919f8eff64defb284723d31e9f05186115c5
Paweł Pacana <pawel.pacana@gmail.com>
2011-06-30 19:27:14 +0200

    regenerated dumy app for 3.1.0.rc4

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'cbebed14866ec19bfb972ef32fbe18f45430310602f9737ff0abdbbc4f658ce7bd7b92dc7fcbd7d27f454aa70f88093635719e8481638223eca5294a87ceae58'
+Dummy::Application.config.secret_token = 'cd23943cb8e7a2d0221e680b77749f23b97c90158c0749ed989c9e938382f102b569460413fc40b9cca5445972a9fa2af1b47e299d7f7e22e70cb5e056fdf8ba'
